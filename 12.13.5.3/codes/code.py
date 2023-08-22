a041e53efd877f0acbbb3e0c3b1db3dbbf45fd73
anupama1412 <141239010+anupama1412@users.noreply.github.com>
2023-08-22 19:23:27 +0530

    Add files via upload

---
+++ 12.13.5.3/codes/code.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import binom
+
+# Parameters for the binomial distribution
+n = 10  # Number of trials
+p = 0.05  # Probability of success
+
+# Generate the probability mass function (PMF) for various values of k (number of successes)
+p0 = binom.pmf(0, n, p)
+p1 = binom.pmf(1, n, p)
+
+print(f'P(X = 0) =', p0)
+print(f'P(X = 1) =', p1)
+
+print('Total probability =', p0+p1)
