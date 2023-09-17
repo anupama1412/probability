e8c7a65d6ada8250757e459ea3359da738f2d557
anupama1412 <141239010+anupama1412@users.noreply.github.com>
2023-09-17 08:56:07 +0530

    Add files via upload

--- 12.13.3.1/codes/code.py
+++ 12.13.3.1/codes/code.py
@@ -1,70 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import bernoulli
 
-#Probability of the event of getting same number on both dice, i.e., W=0 is 1/6
-prob = 0.2 
-size = int(1/prob)
+# Define the probability distribution for the loaded die
+loaded_die_probs = [0.2, 0.2, 0.1, 0.3, 0.1, 0.1]  
 
-#Number of samples is 10^(size-1), 0 denotes same number on dice
-simlen=int(100000)
+# Simulate rolling the loaded die twice
+num_rolls = 10000  # You can adjust the number of rolls as needed
+X = np.random.choice(np.arange(1, 7), num_rolls, p=loaded_die_probs)
+Y = np.random.choice(np.arange(1, 7), num_rolls, p=loaded_die_probs)
 
-#Generating sample date using Bernoulli r.v.
-data_bern = bernoulli.rvs(size=simlen,p=prob)
+# Calculate Z as the sum of X and Y
+Z = X + Y
+W = X - Y
 
-print("Samples generated:",data_bern)
+# Calculate the probabilities for each value of W
+unique_values, counts = np.unique(W, return_counts=True)
+probabilities = counts / num_rolls
 
-def generate_pmf(size):
-	array = np.full(size, prob)
-	return array
+# Create a stem plot
+plt.figure(figsize=(8, 6))
+plt.stem(np.unique(Z), np.bincount(Z)[np.unique(Z)] / num_rolls, markerfmt='ro', basefmt=' ', linefmt='k-')
+plt.title('Stem Plot of Z = X + Y (Two Rolls of Loaded Die)')
+plt.xlabel('Z (Sum of X and Y)')
+plt.ylabel('Probability')
+plt.xticks(np.arange(2, 13))
+plt.grid(True)
+plt.savefig('../figs/Z.png')
 
-pmf_X = generate_pmf(size)
-pmf_Y = generate_pmf(size)
-pmf_W_act = np.convolve(pmf_X, pmf_Y, mode='full')
-
-# Calculate the actual probability of getting the same number on both dice (W = 0)
-act_prob_same_no = pmf_W_act[5]  # Probability at W = 0
-# Calculate the actual probability of getting different numbers on both dice (|W| > 0)
-act_prob_diff_nos = 1 - act_prob_same_no
-
-# Simulate the random variables X and Y by rolling two dice
-num_samples = simlen
-X_simulated = np.random.randint(1, size + 1, num_samples)
-Y_simulated = np.random.randint(1, size + 1, num_samples)
-
-# Calculate W for each pair of X and Y
-W_simulated = X_simulated - Y_simulated
-pmf_W_sim = np.bincount(W_simulated + size - 1) / num_samples
-
-# Calculate the simulated probability of getting the same number on both dice (W = 0)
-sim_prob_same_no = pmf_W_sim[5]  # Probability at W = 0
-# Calculate the simulated probability of getting different numbers on both dice (|W| > 0)
-sim_prob_diff_nos = 1 - sim_prob_same_no
-
-print("Actual Probability (W = 0):", act_prob_same_no)
-print("Simulated Probability (W = 0):", sim_prob_same_no)
-
-if np.isclose(act_prob_same_no, act_prob_same_no):
-    print("Hence verified for same number on both dice")
-else:
-    print("error")
-
-print("Actual Probability (W ≠ 0):", act_prob_diff_nos)
-print("Simulated Probability (W ≠ 0):", sim_prob_diff_nos)
-
-if np.isclose(act_prob_diff_nos, act_prob_diff_nos):
-    print("Hence verified for different numbers on both dice")
-else:
-    print("error")
-
-k_values = np.arange(-size + 1, size) 
-   
-plt.stem(k_values, pmf_W_act, markerfmt='o', linefmt='C0-', use_line_collection=True, label='Actual')
-plt.stem(k_values, pmf_W_sim, markerfmt='o', linefmt='C1-', use_line_collection=True, label='Simulation')
-plt.xlabel("k")
-plt.ylabel("Probability (simulated vs actual)")
-plt.legend()
-plt.grid()
-plt.savefig('../figs/fig.png')
-plt.show()
+plt.figure(figsize=(8, 6))
+plt.stem(unique_values, probabilities, markerfmt='ro', basefmt=' ', linefmt='k-')
+plt.title('Stem Plot of W = X - Y (Two Rolls of Loaded Die)')
+plt.xlabel('W (Difference between X and Y)')
+plt.ylabel('Probability')
+plt.xticks(np.arange(-5, 6))
+plt.grid(True)
+plt.savefig('../figs/W.png')
 
